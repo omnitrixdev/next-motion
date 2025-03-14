970e6b410e75e18a5e4b1ad5335b682578c52a3f
mhmadamrii <interceptorghost4@gmail.com>
2025-03-14 23:45:24 +0700

    .

--- src/app/testing/page.tsx
+++ src/app/testing/page.tsx
@@ -4,43 +4,24 @@ import { motion } from 'motion/react';
 import { useState } from 'react';
 
 export default function Testing() {
-  const [isShow, setIsShow] = useState(false);
-  const transition = {
-    duration: 0.8,
-    delay: 0.5,
-    ease: [0, 0.71, 0.2, 1.01],
-  };
-
   return (
-    <section>
-      <h1>Hello world</h1>
-      <div className='w-[400px] h-[400px]'>
-        {isShow && (
-          <motion.div
-            whileHover={{
-              scale: 1.1,
-              transition: { duration: 0.2 },
-            }}
-            animate={{ x: 100 }}
-            transition={transition}
-            className='w-[300px] h-[300px] bg-blue-400'
-          >
-            <p className='text-white'>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Cupiditate corrupti sint est labore, porro praesentium eligendi
-              modi at assumenda molestias!
-            </p>
-          </motion.div>
-        )}
-      </div>
-      <motion.button
-        onClick={() => setIsShow(!isShow)}
-        className='bg-blue-500 p-2.5 rounded-md'
-        whileTap='tap'
-        whileHover='hover'
+    <section className='container mx-auto flex justify-center items-center flex-col'>
+      <h1>This is supposed to be some motions</h1>
+      <motion.div
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }}
+        className='w-[300px] h-[200px]'
       >
-        Hello world
-      </motion.button>
+        Hello, Motion!
+      </motion.div>
+      <div className='border'>
+        <motion.div
+          drag
+          dragConstraints={{ left: 60, right: 100, top: 100, bottom: 100 }}
+          className='bg-green-500 w-[500px] h-[500px]'
+        />
+      </div>
     </section>
   );
 }
